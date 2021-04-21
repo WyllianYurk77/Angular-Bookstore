bce44ffaf90be0e8086d738ec88521c9ba8e1f6f
WyllianYurk77 <wyllianyurk77@gmail.com>
2021-04-21 14:09:00 -0300

    Criando e implementando o componente Categoria Create

--- src/app/components/views/category/category.service.ts
+++ src/app/components/views/category/category.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Category } from './category.model';
@@ -11,10 +12,23 @@ export class CategoryService {
 
   baseUrl: String = environment.baseUrl
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private snackbar: MatSnackBar) { }
 
-  findAll():Observable<Category[]> {
+  findAll(): Observable<Category[]> {
     const url = `${this.baseUrl}/categorias`
     return this.http.get<Category[]>(url)
+  }
+
+  create(category: Category): Observable<Category> {
+    const url = `${this.baseUrl}/categorias`
+    return this.http.post<Category>(url, category)
+  }
+
+  alert(str: String): void {
+    this.snackbar.open(`${str}`, 'Ok', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    })
   }
 }
